b17d1c3d20557d47053c20b8ffe6b412e1085514
Predeshen Naidoo <predeshen@gmail.com>
2020-07-29 20:10:27 +0200

    icx

---
+++ Extentions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskListApp.Data;
+
+namespace TaskListApp.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        //public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        //{
+        //    var connectionString = config["mysqlconnection:connectionString"];
+        //    services.AddDbContext<TaskListAppDbEntities>(o => o.UseMySql(connectionString));
+        //}
+
+
+    }
+    
+}
---
+++ Models/TaskDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskListApp.Models
+{
+    public class TaskDetails
+    {
+        [Key]
+        public string TaskId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Description { get; set; }
+
+        public char Status { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
